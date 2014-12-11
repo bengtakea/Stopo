12e6fe3bdb9610a0c46c1f8edbe3445e1c4311d0
bengtakea <ba1@velocius.se>
2014-12-11 12:26:10 +0100

    hh

--- Stopo/WebContent/js/controllers.js
+++ Stopo/WebContent/js/controllers.js
@@ -7,4 +7,10 @@ var stopoControllers = angular.module('stopoControllers', []);
 stopoControllers.controller('ListCtrl', [ '$scope', 'Portfolio',
 		function($scope, Portfolio) {
 			$scope.stocks = Portfolio.query();
+
+			$scope.AddTicker = function(ticker) {
+				alert("Hej");
+//				Portfolio.Add(ticker);
+			};
+
 		} ]);
