9a83c9f05bea97df53af46ab7f4d57638eb763f0
bengtakea <ba1@velocius.se>
2014-12-16 19:47:07 +0100

    gg

--- Stopo/WebContent/js/controllers.js
+++ Stopo/WebContent/js/controllers.js
@@ -4,21 +4,85 @@
 
 var stopoControllers = angular.module('stopoControllers', []);
 
-stopoControllers.controller('ListCtrl', [ '$scope', 'Portfolio', 'Stock',
-		function($scope, Portfolio, Stock) {
-			$scope.refresh = function() {
-				$scope.stocks = Portfolio.query();
-			};
+stopoControllers
+		.controller(
+				'ListCtrl',
+				[
+						'$scope',
+						'$http',
+						'Portfolio',
+						'Stock',
+						'Yahoo',
+						function($scope, $http, Portfolio, Stock, Yahoo) {
+							$scope.refresh = function() {
+								$scope.stocks = Portfolio.query();
+								var i;
+								for (i = 0; i < $scope.stocks.length; i++) {
+									if ($scope.stocks[i].label == 'gold') {
+										$scope.stocks[i].label = 'Guld'
+									}
+									if ($scope.stocks[i].label == 'energy') {
+										$scope.stocks[i].label = 'Energi'
+									}
+									if ($scope.stocks[i].label == 'teck') {
+										$scope.stocks[i].label = 'Teck'
+									}
+									if ($scope.stocks[i].label == 'general') {
+										$scope.stocks[i].label = 'Allmänt'
+									}
+								}
+							};
 
-			$scope.AddTicker = function() {
-				var stock = $scope.stock;
-				stock.name = 'Aktie-namn';
-				Stock.save($scope.stock, function(data) {
-					$scope.refresh();
-				});
+							$scope.AddTicker = function() {
+								var stock = {};
+								Yahoo
+										.get(
+												{
+													symbol : $scope.newTicker
+												},
+												function(data) {
+													$scope.error = false;
+													stock.yahooTicker = $scope.newTicker;
+													stock.label = $scope.newLabel;
+													stock.name = data.query.results.quote["Name"];
+													Stock.save(stock, function(
+															data) {
+														$scope.refresh();
+													});
+													$scope.newTicker = "";
+												}, function(response) {
+													$scope.error = true;
+												});
 
-			};
+							};
 
-			$scope.refresh();
+							$scope.UpdateQuotes = function() {
+								var i;
+								for (i = 0; i < $scope.stocks.length; i++) {
+									Yahoo
+											.get(
+													{
+														symbol : $scope.stocks[i].yahooTicker
+													},
+													function(data) {
+														$scope.error = false;
+														var stock = {};
+														stock.yahooTicker = data.query.results.quote["symbol"];
+														var j;
+														for (j = 0; j < $scope.stocks.length; j++) {
+															if ($scope.stocks[j].yahooTicker == stock.yahooTicker) {
+																$scope.stocks[j].lastQuote = data.query.results.quote["LastTradePriceOnly"];
+																$scope.stocks[j].change = data.query.results.quote["Change"];
+															}
+														}
+													}, function(response) {
+														$scope.error = true;
+													});
 
-		} ]);
+								}
+							};
+
+							$scope.newLabel = 'gold';
+							$scope.refresh();
+
+						} ]);
--- Stopo/WebContent/js/services.js
+++ Stopo/WebContent/js/services.js
@@ -5,11 +5,21 @@
 var stopoServices = angular.module('stopoServices', [ 'ngResource' ]);
 
 stopoServices.factory('Portfolio', [ '$resource', function($resource) {
-	return $resource('rest/stoposervice/getstocks', {}, {
-	});
+	return $resource('rest/stoposervice/getstocks', {}, {});
 } ]);
 
 stopoServices.factory('Stock', [ '$resource', function($resource) {
-	return $resource('rest/stoposervice/stock/:id', {}, {
-	});
+	return $resource('rest/stoposervice/stock/:id', {}, {});
 } ]);
+
+stopoServices
+		.factory(
+				'Yahoo',
+				[
+						'$resource',
+						function($resource) {
+							return $resource(
+									"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D':symbol"
+											+ "'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+									{}, {});
+						} ]);
